9e8c60f23a4d3bdb65fc6e705d22fb87df154b91
ljavadovaa <ljavadovaa@gmail.com>
2020-02-15 20:29:55 +0400

    #edited-ShootingSquareApp

--- src/main/java/hw02/ShootingSquareApp.java
+++ src/main/java/hw02/ShootingSquareApp.java
@@ -6,51 +6,57 @@ public class ShootingSquareApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("All set. Get ready to rumble!");
-        char[][] a = new char[25][25];
-        int[] num1 = new int[25];
-        int[] num2 = new int[25];
-        int max=5;
-        boolean flag = true;
-        int rand1 = (int) (Math.random()*(max+1));
-        int rand2 = (int) (Math.random()*(max+1));
-        System.out.printf("%d %d\n",rand1,rand2);
+        String[][] a = new String[20][20];
+        int[] number1 = new int[20];
+        int[] number2 = new int[20];
+        final int MAX=5;
+        int random1 = (int) (Math.random()*MAX+1);
+        int random2 = (int) (Math.random()*MAX+1);
+        //System.out.printf("%d %d\n",random1,random2);
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                a[i][j]='-';
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                a[i][j]="-";
             }
         }
 
-        while (flag)
-        {
-            int i=1,j=1;
-            num1[i]=sc.nextInt();
-            if(num1[i]<1 || num1[i]>5) System.out.println("Please, enter number in the range of 1-5");
-            num2[j]=sc.nextInt();
-            if(num2[i]<1 || num2[i]>5) System.out.println("Please, enter number in the range of 1-5");
+        for (int i = 0; i < 6; i++) {
+            a[0][i]= String.valueOf(i);
+        }
+        for (int j = 0; j < 6; j++) {
+            a[j][0]= String.valueOf(j);
+        }
 
-            if(rand1==num1[i] && rand2==num2[j])
+        while (true)
+        {
+            int i=0;
+            number1[i]=sc.nextInt();
+            number2[i]=sc.nextInt();
+            if(!(number1[i]>=1 && number1[i]<=5) || !(number2[i]>=1 && number2[i]<=5))
+                System.out.println("Please, enter number in the range of 1-5");
+
+            if(random1==number1[i] && random2==number2[i])
             {
-                a[num1[i]][num2[j]]='x';
+                a[number1[i]][number2[i]]="x";
                 System.out.println("You have won!");
-                flag=false;
                 break;
             }
             else
             {
-                a[num1[i]][num2[j]]='*';
-                for (int m = 1; m <= 5; m++) {
-                    for (int n = 1; n <= 5; n++) {
+                if (number1[i]==0 || number2[i]==0) continue;
+                a[number1[i]][number2[i]]="*";
+                for (int m = 0; m <= 5; m++) {
+                    for (int n = 0; n <= 5; n++) {
                         System.out.printf("%s ",a[m][n]);
                     }
                     System.out.println();
                 }
             }
-            i++; j++;
+            i++;
         }
 
-        for (int m = 1; m <= 5; m++) {
-            for (int n = 1; n <= 5; n++) {
+        for (int m = 0; m <= 5; m++) {
+            for (int n = 0; n <= 5; n++) {
                 System.out.printf("%s ",a[m][n]);
             }
             System.out.println();
