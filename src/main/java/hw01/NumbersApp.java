94d5f098c43197bdd9f9364de2e85cf5105c724b
ljavadovaa <ljavadovaa@gmail.com>
2020-02-15 20:28:42 +0400

    #edited-NumbersApp

--- src/main/java/hw01/NumbersApp.java
+++ src/main/java/hw01/NumbersApp.java
@@ -4,25 +4,27 @@ import java.util.Scanner;
 
 public class NumbersApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Let the game begin!");
-        String name = "Leman";
-        int[] a = new int[100];
-        int max = 100,i;
-        int rand = (int) (Math.random()*(max+1));
-        boolean flag = true;
-        while (flag)
+        System.out.println("Please, enter player's name");
+        String name = input.next();
+        System.out.println("Please, enter any number between 0-100");
+        final int MAX = 100;
+        int[] a = new int[MAX];
+        int i;
+        int random = (int) (Math.random()*(MAX+1));
+        while (true)
         {
             i = 0;
-            a[i] = sc.nextInt();
-            if (a[i] < rand)
+            a[i] = input.nextInt();
+            if (a[i] < random)
                 System.out.println("Your number is too small. Please, try again.");
-            else if (a[i] > rand)
+            else if (a[i] > random)
                 System.out.println("Your number is too big. Please, try again.");
             else
             {
-                flag = false;
                 System.out.printf("Congratulations, %s!",name);
+                break;
             }
             i++;
         }
